1f2ba7e6e068a26e329d3f164dda3f1e862811fa
Ильдар Ибрагимов <113426639+RuCyberneticc@users.noreply.github.com>
2023-03-24 14:54:16 +0300

    Create task24

---
+++ homework24/task24/task24.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String s = input.nextLine();
+        String[] arr = s.split(" ");
+        int[] arr2 = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            arr2[i] = Integer.parseInt(arr[i]);
+        }
+        int m = arr2[0];
+        int n = arr2[1];
+        int sm = 0;
+        for (int i = m; i <= n; i++) {
+            boolean flag = false;
+            for (int j = 2; j < arr2.length; j++) {
+                if (i % arr2[j] == 0) {
+                    flag = true;
+                }
+            }
+            if (flag) {
+                sm += i;
+            }
+        }
+        System.out.println(sm);
+
+    }
+
+}
